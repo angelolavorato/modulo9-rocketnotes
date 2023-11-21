f2bbeac73e30c862dc6369cb32ef5b4cf114d68d
angelolavorato <angelolavorato@hotmail.com>
2023-11-20 21:54:47 -0300

    criando um botão como componente global

---
+++ rocketnotes/src/components/Button/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles";
+
+export function Button(){
+
+  return(
+
+    <Container type="button">
+    Meu butão
+  </Container>
+    )
+
+
+}
--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
-import { Container } from "./styles"
+import { Container } from "./styles";
+
+import { Button } from "../../components/Button";
 
 export function Details() {
   return (
     <Container>
       <h1>Alo</h1>
       <span>JiL000</span>
+
+      <Button/>
     </Container>
   )
 }
