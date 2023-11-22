ed0a34b9adffa7d0b84978e2b222270d3482d0f4
angelolavorato <angelolavorato@hotmail.com>
2023-11-21 22:44:24 -0300

    adicionando a biblioteca de ícones do react e estilizando um botão de sair

--- rocketnotes/src/components/Header/index.jsx
+++ rocketnotes/src/components/Header/index.jsx
@@ -1,4 +1,6 @@
-import { Container, Profile } from './styles'
+import {RiShutDownLine}from'react-icons/ri'
+
+import { Container, Profile, Logout } from './styles'
 
 export function Header() {
   return (
@@ -13,6 +15,10 @@ export function Header() {
           <strong>Ângelo Soldati Lavorato</strong>
         </div>
 </Profile>
+
+<Logout>
+  <RiShutDownLine/>
+</Logout>
     </Container>
   )
 }
