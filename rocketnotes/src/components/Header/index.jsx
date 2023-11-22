a7717844dd2f611cfd84287ab74304066b5e8579
angelolavorato <angelolavorato@hotmail.com>
2023-11-21 22:32:46 -0300

    adicionando a foto de usuário e estilizando

--- rocketnotes/src/components/Header/index.jsx
+++ rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,18 @@
-import { Container } from './styles'
+import { Container, Profile } from './styles'
 
 export function Header() {
   return (
     <Container>
+<Profile>
+  <img src='https://github.com/angelolavorato.png'
+        alt="Foto de usuario"
+        />
 
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Ângelo Soldati Lavorato</strong>
+        </div>
+</Profile>
     </Container>
   )
 }
