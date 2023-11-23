1b0c96251bf4e6e60926db541240ffd6492fdf3d
angelolavorato <angelolavorato@hotmail.com>
2023-11-22 22:00:00 -0300

    utilizando o elemento de children para a listagem de componentes

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -2,11 +2,21 @@ import { Container } from './styles'
 
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
+
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+<Section title="Links úteis">
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+  </Section>      
       <Button title="Voltar" />
     </Container>
   )
