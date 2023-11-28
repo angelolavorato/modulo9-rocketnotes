d780b0d8381b6fcef574ea585aa8d083a7c43ce3
angelolavorato <angelolavorato@hotmail.com>
2023-11-27 22:09:38 -0300

    ajustando o isactive

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -14,7 +14,7 @@ export function Home(){
       <Header/>
 <Menu>
 
-<li>  <ButtonText title="Todos" isActive/></li>
+<li>  <ButtonText title="Todos" $isactive/></li>
 <li><ButtonText title="React"/></li>
 <li><ButtonText title="Node"/></li>
 
