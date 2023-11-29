6c43acefbae3cf2d73853b77d61804add271909a
angelolavorato <angelolavorato@hotmail.com>
2023-11-28 22:07:08 -0300

    criando a pagina de signin

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
-import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles/>
-      <Home />
+      <GlobalStyles />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
