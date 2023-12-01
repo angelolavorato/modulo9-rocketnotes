e808b80cf04fb46e607f9bb7d180b774a2b2c603
angelolavorato <angelolavorato@hotmail.com>
2023-11-30 22:24:20 -0300

    Importando a imagem para a pagina de signin

--- rocketnotes/src/pages/SignIn/index.jsx
+++ rocketnotes/src/pages/SignIn/index.jsx
@@ -1,37 +1,37 @@
-import {FiMail, FiLock} from 'react-icons/fi';
+import { FiMail, FiLock } from 'react-icons/fi'
 
-import {Input}from '../../components/Input';
-import {Button}from '../../components/Button';
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
 
+import { Container, Form, Background } from './styles'
 
-import { Container, Form } from './styles';
-
-export function SignIn(){
-  return(
+export function SignIn() {
+  return (
     <Container>
       <Form>
         <h1>Rocket Notes</h1>
-        <p>Aplicação para salvar e gerenciar seus links úteis</p>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
         <h2>Faça seu login</h2>
 
         <Input
-        placeholder="E-mail"
-        type="text"
-        icon={FiMail}
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
         />
 
-<Input
-        placeholder="Senha"
-        type="password"
-        icon={FiLock}
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
         />
-<Button title="Entrar"/>
-<a href="#">
-  Criar Conta
-</a>
 
+        <Button title="Entrar" />
+
+        <a href="#">Criar conta</a>
       </Form>
+
+      <Background />
     </Container>
-  );
+  )
 }
