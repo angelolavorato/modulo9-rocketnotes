0202d01ee612e389e9a8c64472f3f2088aa2bf47
angelolavorato <angelolavorato@hotmail.com>
2023-12-05 22:26:12 -0300

    estilizando e adicionando o avatar de usuário

--- rocketnotes/src/pages/Profile/index.jsx
+++ rocketnotes/src/pages/Profile/index.jsx
@@ -1,47 +1,61 @@
-import {FiArrowLeft, FiUser, FiMail, FiLock}from 'react-icons/fi';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 
-import{Input} from'../../components/Input'
-import{Button} from'../../components/Button'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
 
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
- 
- export function Profile(){
-  return(
+export function Profile() {
+  return (
     <Container>
       <header>
         <a href="/">
-          <FiArrowLeft/>
+          <FiArrowLeft />
         </a>
       </header>
 
-        <Form>
-          <Input
+      <Form>
+        <Avatar>
+          <img
+            src="https://github.com/angelolavorato.png"
+            alt="Foto do usuário"
+          />
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input
+              id="avatar"
+              type="file"
+            />
+          </label>
+        </Avatar>
+
+        <Input
           placeholder="Nome"
           type="text"
           icon={FiUser}
-          />
+        />
 
-<Input
+        <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
-          />
+        />
 
-<Input
+        <Input
           placeholder="Senha atual"
           type="password"
           icon={FiLock}
-          />
+        />
 
-<Input
-          placeholder="Nova senha"
+        <Input
+          placeholder="Nova atual"
           type="password"
           icon={FiLock}
-          />
+        />
 
-          <Button title="Salvar"/>        
-        </Form>
+        <Button title="Salvar" />
+      </Form>
     </Container>
   )
- }
+}
